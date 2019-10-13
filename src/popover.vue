dfb961aed66822809070f5e0dba2d94ec844392e
weihu <1183969348@qq.com>
2019-10-13 22:09:05 +0800

    解决当容器有 overflow hidden 造成的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="popover" @click.stop="xxx" >
-    <div class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click.stop="xxx">
+    <div
+      ref="contentWrapper"
+      class="content-wrapper"
+      v-if="visible"
+      @click.stop
+    >
       <slot name="content"></slot>
     </div>
+    <span ref="triggerWrapper"></span>
     <slot></slot>
   </div>
 </template>
@@ -12,20 +18,31 @@ export default {
   data() {
     return { visible: false };
   },
+  mounted() {
+    console.log(this.$refs.triggerWrapper);
+  },
   methods: {
     xxx() {
       this.visible = !this.visible;
-      if(this.visible === true){
-        this.$nextTick(()=>{
-            let eventHandler = () =>{
-               this.visible = false 
-               console.log('document 隐藏 popover')
-               document.removeEventListener('click',eventHandler)
-            }
-            document.addEventListener('click',eventHandler)
-        })
-      }else{
-      console.log('vm隐藏 popover')
+      if (this.visible === true) {
+        this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper);
+          let {
+            width,
+            height,
+            top,
+            left
+          } = this.$refs.triggerWrapper.getBoundingClientRect();
+          this.$refs.contentWrapper.style.left=left + window.scrollX+ 'px'
+          this.$refs.contentWrapper.style.top=top + window.scrollY+'px'
+          let eventHandler = () => {
+            this.visible = false;
+            document.removeEventListener("click", eventHandler);
+          };
+          document.addEventListener("click", eventHandler);
+        });
+      } else {
+        console.log("vm隐藏 popover");
       }
     }
   }
@@ -36,12 +53,11 @@ export default {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  .content-wrapper {
-      position: absolute;
-      bottom:100%;
-      left: 0;
-      border: 1px solid red;
-      box-shadow: 0 0 3px rgba(0,0,0,.5)
-  }
 }
+  .content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%)
+  }
 </style>
