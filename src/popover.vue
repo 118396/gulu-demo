8f497e10c613c397bc391b65e04153d26e38aebe
weihu <1183969348@qq.com>
2019-10-14 00:42:40 +0800

    添加测试用例

--- src/popover.vue
+++ src/popover.vue
@@ -1,53 +1,119 @@
 <template>
-  <div class="popover" @click="onClick" ref="popover">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
-      <slot name="content"></slot>
+  <div class="popover" ref="popover">
+    <div
+      ref="contentWrapper"
+      class="content-wrapper"
+      v-if="visible"
+      :class="{ [`position-${position}`]: true }"
+    >
+      <slot name="content" :close="close"></slot>
     </div>
-    <span ref="triggerWrapper"></span>
-    <slot></slot>
+    <span ref="triggerWrapper" style="display: inline-block;">
+      <slot></slot>
+    </span>
   </div>
 </template>
+
 <script>
 export default {
   name: "GuluPopover",
+  props: {
+    position: {
+      type: String,
+      default: "top",
+      validator(value) {
+        return ["top", "bottom", "left", "right"].indexOf(value) >= 0;
+      }
+    },
+    trigger: {
+      type: String,
+      default: "click",
+      validator(value) {
+        return ["click", "hover"].indexOf(value) >= 0;
+      }
+    }
+  },
   data() {
-    return { visible: false };
+    return {
+      visible: false
+    };
   },
   mounted() {
-    console.log(this.$refs.triggerWrapper);
+    if (this.trigger === "click") {
+      this.$refs.popover.addEventListener("click", this.onClick);
+    } else {
+      this.$refs.popover.addEventListener("mouseenter", this.open);
+      this.$refs.popover.addEventListener("mouseleave", this.close);
+    }
+  },
+  destroyed() {
+    if (this.trigger === "click") {
+      this.$refs.popover.removeEventListener("click", this.onClick);
+    } else {
+      this.$refs.popover.removeEventListener("mouseenter", this.open);
+      this.$refs.popover.removeEventListener("mouseleave", this.close);
+    }
+  },
+  computed: {
+    openEvent() {
+      if (this.trigger === "click") {
+        return "click";
+      } else {
+        return "mouseenter";
+      }
+    },
+    closeEvent() {
+      if (this.trigger === "click") {
+        return "click";
+      } else {
+        return "mouseleave";
+      }
+    }
   },
   methods: {
-        positionContent () {
-        const {contentWrapper, triggerWrapper} = this.$refs
-        document.body.appendChild(contentWrapper)
-        const {width, height, top, left} = triggerWrapper.getBoundingClientRect()
-        const {height: height2} = contentWrapper.getBoundingClientRect()
-        let positions = {
-          top: {top: top + window.scrollY, left: left + window.scrollX,},
-          bottom: {top: top + height + window.scrollY, left: left + window.scrollX},
-          left: {
-            top: top + window.scrollY + (height - height2) / 2,
-            left: left + window.scrollX
-          },
-          right: {
-            top: top + window.scrollY + (height - height2) / 2,
-            left: left + window.scrollX + width
-          },
+    positionContent() {
+      const { contentWrapper, triggerWrapper } = this.$refs;
+      document.body.appendChild(contentWrapper);
+      const {
+        width,
+        height,
+        top,
+        left
+      } = triggerWrapper.getBoundingClientRect();
+      const { height: height2 } = contentWrapper.getBoundingClientRect();
+      let positions = {
+        top: { top: top + window.scrollY, left: left + window.scrollX },
+        bottom: {
+          top: top + height + window.scrollY,
+          left: left + window.scrollX
+        },
+        left: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + window.scrollX
+        },
+        right: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + window.scrollX + width
         }
-        contentWrapper.style.left = positions[this.position].left + 'px'
-        contentWrapper.style.top = positions[this.position].top + 'px'
-      },,
+      };
+      contentWrapper.style.left = positions[this.position].left + "px";
+      contentWrapper.style.top = positions[this.position].top + "px";
+    },
     onClickDocument(e) {
       if (
         this.$refs.popover &&
         (this.$refs.popover === e.target ||
           this.$refs.popover.contains(e.target))
-      ) {return;}
+      ) {
+        return;
+      }
       if (
         this.$refs.contentWrapper &&
         (this.$refs.contentWrapper === e.target ||
           this.$refs.contentWrapper.contains(e.target))
-      ) { return;}
+      ) {
+        return;
+      }
       this.close();
     },
     open() {
